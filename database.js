7fed0122794608bdc049ce707c989ca3f8682e90
Somaraju Thonta <159985115+SAISOMARAJUTHONTA@users.noreply.github.com>
2025-01-11 09:47:17 +0530

    database.js

--- database.js
+++ database.js
@@ -1,15 +1,12 @@
-const { Pool } = require("pg");
+// database.js
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "sportscheduler",
-  password: "changename",
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'sports_scheduler',
+    password: '1811',
+    port: 5432,
 });
-pool
-  .connect()
-  .then(() => console.log("PostgreSQL connected"))
-  .catch((err) => console.error("Error connecting to PostgreSQL", err));
 
 module.exports = pool;
